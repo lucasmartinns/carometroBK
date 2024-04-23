f5d7c17b3fc9d8e069c25f6c4f0ae8e9f4f8472b
lucasmartinns <lucasalvesmartinns@gmail.com>
2024-04-23 16:48:22 -0300

    teste

---
+++ config/sequelize.js
@@ -0,0 +1,10 @@
+// Iniicia o Sequelize
+
+const Sequelize = require('sequelize');
+const databaseConfig = require('./database');
+
+const Sequelize = new Sequelize(databaseConfig.development);
+
+//Agora você pode usar o objeto "sequelize" ára interagir com o banco de dados
+
+module.exports = sequelize
